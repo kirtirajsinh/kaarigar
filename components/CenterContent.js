6fc7398ad3fd2aa34ebc28335717221e7f2090fe
Meta-K <kirtiraj3738@gmail.com>
2022-10-31 23:55:34 +0530

    Add center Content

---
+++ components/CenterContent.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Image from 'next/image'
+
+const CenterContent = () => {
+  return (
+    <div className="flex flex-col md:flex-row items-center justify-center m-0.5">
+      <section className="flex flex-col">
+        <p className="font-Heading font-bold text-6xl leading-tight">
+            POST WORK, FIND TALENT,<br></br> & PAY AS THEY MAKE PROGRESS. 
+      </p>
+      <p className="font-medium">
+          Find Work and get paid as you Progress - Freelancers paradise.
+      </p>
+      <button className=" w- h-10 text-center drop-shadow-lg text-base border border-solid border-black border-2  self-center my-3">Launch App</button>
+      
+      </section>
+      <Image src="/peopleWorking.png" alt="people working" width={800} height={800} />
+    </div>
+  )
+}
+
+export default CenterContent
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import CenterContent from '../components/CenterContent'
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-          Hello world!
-      </h1>
+      <CenterContent />
     </div>
   )
 }
