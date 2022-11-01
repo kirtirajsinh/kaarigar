b465d3e3e2e24b826f14814849d6c21ab1a1c030
Meta-K <kirtiraj3738@gmail.com>
2022-11-01 22:31:53 +0530

    add rainbow kit

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-          Hello world!
-      </h1>
+      
+          <ConnectButton />
+     
     </div>
   )
 }
